07f693e4ed9095e0d1a0484610818fc2bfc46396
dreamagici4n <lucascostamr812@gmail.com>
2023-10-12 17:49:32 -0300

    feat: algoritimo Round-Robin terminado

--- src/Algoritmos/RR.js
+++ src/Algoritmos/RR.js
@@ -7,8 +7,6 @@
 */
 
 function calculateRR(processos, quantum) {
-    const sortedProcessos = ajustarDuracaoQuantum(processos, quantum).sort((a, b) => a.tempoDeChegada - b.tempoDeChegada);
-
     const resultado = [];
 
     let tempoAtual = 0;
@@ -17,26 +15,30 @@ function calculateRR(processos, quantum) {
     let trocasDeContexto = -1;
     let label = "Round Robin";
     
-    while(sortedProcessos.length > 0) {
-        if (sortedProcessos.length === 0) {
-            tempoAtual++;
-        }
-        const proximoProcesso = sortedProcessos.shift();
-        const startTime = proximoProcesso.tempoDeChegada;
-        const endTime = startTime + proximoProcesso.duracao;
-        const waitTime = Math.abs(proximoProcesso.duracao - proximoProcesso.tempoDeChegada);
+    const newProcessos = normalize(processos, quantum);
+    
+    // while(newProcessos.length > 0) {
+    //     if (newProcessos.length === 0) {
+    //         tempoAtual++;
+    //     }
+    //     const proximoProcesso = newProcessos.shift();
+    //     const startTime = proximoProcesso.tempoDeChegada;
+    //     const endTime = startTime + proximoProcesso.duracao;
 
-        resultado.push({
-            label: proximoProcesso.label,
-            times: [{ startTime, duration: proximoProcesso.duracao }],
-            waitTimes: [{ startTime: proximoProcesso.tempoDeChegada, duration: waitTime }],
-        });
+    //     //waitime 
+    //     const waitTime = Math.abs(startTime - newProcessos.length);
 
-        tempoTotalExecucao += endTime - startTime;
-        tempoTotalEspera += waitTime;
-        tempoAtual += quantum;
-        trocasDeContexto++;
-    }
+    //     resultado.push({
+    //         label: proximoProcesso.label,
+    //         times: [{ startTime, duration: proximoProcesso.duracao }],
+    //         waitTimes: [{ startTime: proximoProcesso.tempoDeChegada, duration: waitTime }],
+    //     });
+
+    //     tempoTotalExecucao += endTime - startTime;
+    //     tempoTotalEspera += waitTime;
+    //     tempoAtual += quantum;
+    //     trocasDeContexto++;
+    // }
 
     // // Ordenar o resultado por label
     resultado.sort((a, b) => a.label.localeCompare(b.label));
@@ -58,35 +60,101 @@ function calculateRR(processos, quantum) {
     return resultadoComMetricas;
 }
 
-function ajustarDuracaoQuantum(processos, quantum) {
-    const newProcessos = [];
+function normalize(processos, quantum) {
+    let newProcessos = [...processos];
 
-    [ ...processos].forEach(function(processo) {
-        let remainingDuration = processo.duracao;
-
-        while (remainingDuration > 0) {
-            if (remainingDuration <= quantum) {
-                processo.duracao = remainingDuration;
-                newProcessos.push({ ...processo });
-                remainingDuration = 0;
-            } else {
-                processo.duracao = quantum;
-                newProcessos.push({ ...processo });
-                remainingDuration -= quantum;
-            }
-        }
-    });
+    newProcessos = ajustarDuracaoQuantum(newProcessos, quantum);
+    newProcessos = ajustarTempoDeChegadaQuantum(newProcessos, quantum);
 
     return newProcessos;
 }
 
-function ajustarTempoDeChegadaQuantum(processos, quantum) {
+function ajustarDuracaoQuantum(processos, quantum) {
     const newProcessos = [];
-    const sortedProcess = processos.sort((a, b) => a.tempoDeChegada - b.tempoDeChegada);
 
+    processos.forEach(function(processo) {
+        let duracaoRestante = processo.duracao;
+
+        while (duracaoRestante > 0) {
+            if (duracaoRestante <= quantum) {
+                processo.duracao = duracaoRestante;
+                newProcessos.push({ ...processo });
+                duracaoRestante = 0;
+            } else {
+                processo.duracao = quantum;
+                newProcessos.push({ ...processo });
+                duracaoRestante -= quantum;
+            }
+        }
+    });
+
+    newProcessos.sort((a, b) => a.tempoDeChegada - b.tempoDeChegada);
+
+    return newProcessos; 
+}
+
+function ajustarTempoDeChegadaQuantum(processos, quantum) {
+    // filtrar pela label
+    let duracaoProcAnterior = 0;
     
-    
-    return newProcessos;
+    const labels = [];
+    processos.forEach(function(processo){
+        if(!labels.includes(processo.label)) {
+            labels.push(processo.label);
+        }
+    });
+
+    //se houver mais de um objeto jogar objetos para o final da fila
+        //o processo pode ser executado complementamente?
+        // processo.tempoDeChegada + quantum < proximoProcesso.tempoDeChegada;
+    const procMaisDeUmObjeto = [];
+    const primeirosProcessos = [];
+    labels.forEach(function(label){
+        let procMesmaLabel = processos.filter((proc) => proc.label === label);
+        if (procMesmaLabel.length > 1) {
+            primeirosProcessos.push(procMesmaLabel.shift());//Remove primeiro elemento do array
+            procMaisDeUmObjeto.push(procMesmaLabel);
+        }
+    });
+
+    // console.log(procMaisDeUmObjeto);
+    for(let i=0; i< procMaisDeUmObjeto.length; i++){
+        let n = 2;
+
+        while(procMaisDeUmObjeto[i][0]?.tempoDeChegada + quantum**n <= primeirosProcessos[i + 1]?.tempoDeChegada) {
+            let processo = procMaisDeUmObjeto[i].shift();
+            primeirosProcessos.push(processo);
+            n++;
+        }
+    }
+
+    primeirosProcessos.sort((a, b)=> a.tempoDeChegada - b.tempoDeChegada);
+
+    //intercalar objetos a mais, um de cada
+    const procFinalFila = [];
+    const maxCont = [...procMaisDeUmObjeto].sort((a, b) => b.length - a.length)[0].length - 1;
+    let cont = 0;
+
+    while(cont <= maxCont) {
+        for(let i = 0; i < procMaisDeUmObjeto.length; i++){
+            let object = procMaisDeUmObjeto[i][cont];
+            if(object !== undefined) procFinalFila.push(object); 
+        }
+        cont++;
+    }
+
+    //Junta os arrays
+    processos = primeirosProcessos.concat(procFinalFila);
+
+    //Seta tempo de chegada com base na duracao do ultimo processo
+    processos.forEach(function(processo) {
+        if (processo.tempoDeChegada < duracaoProcAnterior) {
+            processo.tempoDeChegada = duracaoProcAnterior;
+        }
+        duracaoProcAnterior = quantum + processo.tempoDeChegada;
+    });
+
+    return processos;
 }
 
 export default calculateRR;
