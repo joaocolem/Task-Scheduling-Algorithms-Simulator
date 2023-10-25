6948c4ff485b23ce95566dc04a14cfb7926847ec
João Colem <105292741+joaocolem@users.noreply.github.com>
2023-10-25 16:19:15 -0300

    Update SJF.js

--- src/Algoritmos/SJF.js
+++ src/Algoritmos/SJF.js
@@ -1,10 +1,8 @@
-// sjf.js
 
-// Função para calcular SJF (Shortest Job First)
 function calcularSJF(processos) {
 
     console.log(processos);
-    // Ordenar os processos por tempo de chegada
+
     processos.sort((a, b) => a.tempoDeChegada - b.tempoDeChegada);
   
     let tempoAtual = 0;
@@ -15,17 +13,17 @@ function calcularSJF(processos) {
     let label = "Shortest Job First"
   
     while (processos.length > 0) {
-      // Filtrar os processos que já chegaram
+
       const processosChegados = processos.filter((p) => p.tempoDeChegada <= tempoAtual);
   
       if (processosChegados.length === 0) {
         tempoAtual++;
       } else {
-        // Encontrar o processo com a menor duração entre os que já chegaram
+   
         const menorDuracao = Math.min(...processosChegados.map((p) => p.duracao));
         const proximoProcesso = processosChegados.find((p) => p.duracao === menorDuracao);
   
-        // Calcular os tempos e removê-lo da lista de processos
+
         const startTime = tempoAtual;
         const endTime = startTime + proximoProcesso.duracao;
         const waitTime = startTime - proximoProcesso.tempoDeChegada;
@@ -44,14 +42,13 @@ function calcularSJF(processos) {
       }
     }
   
-    // Ordenar o resultado por label
     resultado.sort((a, b) => a.label.localeCompare(b.label));
   
-    // Calcular o tempo médio de execução e o tempo médio de espera
+
     const tempoMedioExecucao = (tempoTotalExecucao + tempoTotalEspera) / resultado.length;
     const tempoMedioEspera = tempoTotalEspera / resultado.length;
   
-    // Criar o objeto que contém o resultado e as métricas
+
     const resultadoComMetricas = {
       resultado,
       metricas: {
