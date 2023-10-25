fbc9353e7e460d36237c373bd63cb97471d31691
João Colem <105292741+joaocolem@users.noreply.github.com>
2023-10-25 16:18:47 -0300

    Update PRIOc.js

--- src/Algoritmos/PRIOc.js
+++ src/Algoritmos/PRIOc.js
@@ -1,10 +1,9 @@
-// prioc.js
 
-// Função para calcular PRIOc (Escalonamento por prioridade cooperativo)
+
 function calcularPRIOc(processos) {
 
     console.log(processos);
-    // Ordenar os processos por tempo de chegada
+
     processos.sort((a, b) => a.tempoDeChegada - b.tempoDeChegada);
   
     let tempoAtual = 0;
@@ -15,17 +14,17 @@ function calcularPRIOc(processos) {
     let label = "Por prioridade cooperativo";
   
     while (processos.length > 0) {
-      // Filtrar os processos que já chegaram
+      
       const processosChegados = processos.filter((p) => p.tempoDeChegada <= tempoAtual);
   
       if (processosChegados.length === 0) {
         tempoAtual++;
       } else {
-        // Encontrar o processo com a maior prioridade entre os que já chegaram
+      
         const maiorPrioridade = Math.max(...processosChegados.map((p) => p.prioridade));
         const proximoProcesso = processosChegados.find((p) => p.prioridade === maiorPrioridade);
   
-        // Calcular os tempos e removê-lo da lista de processos
+
         const startTime = tempoAtual;
         const endTime = startTime + proximoProcesso.duracao;
         const waitTime = startTime - proximoProcesso.tempoDeChegada;
@@ -44,14 +43,14 @@ function calcularPRIOc(processos) {
       }
     }
   
-    // Ordenar o resultado por label
+   
     resultado.sort((a, b) => a.label.localeCompare(b.label));
   
-    // Calcular o tempo médio de execução e o tempo médio de espera
+
     const tempoMedioExecucao = (tempoTotalExecucao + tempoTotalEspera) / resultado.length;
     const tempoMedioEspera = tempoTotalEspera / resultado.length;
   
-    // Criar o objeto que contém o resultado e as métricas
+
     const resultadoComMetricas = {
       resultado,
       metricas: {
